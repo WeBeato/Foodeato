16957fd0f637a458e29c73482fd25d09b84f3cec
webeato <webeato.ad@gmail.com>
2026-02-08 19:56:19 +0330

    create new recipe form

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,7 +1,58 @@
-import './Create.css'
+import { useState } from "react";
+import "./Create.css";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [metode, setMetode] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(title, metode, cookingTime);
+  }
+
   return (
-    <div>Create</div>
-  )
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            onChange={(e) => {
+              setMetode(e.target.value);
+            }}
+            value={metode}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking Time(m):</span>
+          <input
+            type="number"
+            onChange={(e) => {
+              setCookingTime(e.target.value);
+            }}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
