9b092e629c262f0b7681ee7e788d56d3a7e3b5e4
webeato <webeato.ad@gmail.com>
2026-02-08 19:30:16 +0330

    receive single data

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 import "./Recipe.css";
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isLoading, error } = useFetch(url);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {recipe && (
+          <h1>{recipe.title}</h1>
+      )}
+    </div>
+  );
 }
