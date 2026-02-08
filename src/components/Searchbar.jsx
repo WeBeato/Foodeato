dcffec56f40d8bd8c9c48426c5592f592693e221
webeato <webeato.ad@gmail.com>
2026-02-08 22:17:37 +0330

    add searchbar

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./Searchbar.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Searchbar() {
+  const [term, setTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${term}`);
+  };
+
+  return (
+    <div className="searchbar">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Search">Search:</label>
+        <input
+          id="Search"
+          type="text"
+          onChange={(e) => setTerm(e.target.value)}
+          required
+        />
+      </form>
+    </div>
+  );
+}
