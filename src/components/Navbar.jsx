0836c55a57694ae5670a45af4b3d0ed469d64be8
webeato <webeato.ad@gmail.com>
2026-02-10 16:19:32 +0330

    acces to context value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Foodeato</h1>
