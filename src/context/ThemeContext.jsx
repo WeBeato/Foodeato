23e4fea120a85e3538e3bf5758f2680bebc82dc3
webeato <webeato.ad@gmail.com>
2026-02-10 17:29:14 +0330

    create toggle mode

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -6,6 +6,8 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_COLOR":
       return { ...state, color: action.paylod };
+    case "CHANGE_MODE":
+      return { ...state, mode: action.paylod };
 
     default:
       return state;
@@ -15,12 +17,16 @@ const themeReducer = (state, action) => {
 export function ThemeProvider({ children }) {
   const [state, dispatch] = useReducer(themeReducer, {
     color: "#58249c",
+    mode: "dark",
   });
   const changeColor = (color) => {
     dispatch({ type: "CHANGE_COLOR", paylod: color });
   };
+  const changeMode = (mode) => {
+    dispatch({ type: "CHANGE_MODE", paylod: mode });
+  };
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
